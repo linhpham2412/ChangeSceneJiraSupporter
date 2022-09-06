8298c6e9ce83cadbe33a066da39adfbefd4b6138
Linh <linh.pham@nashtechglobal.com>
2022-09-06 17:58:25 +0700

    Add class SettingData and function save load setting

---
+++ src/main/java/com/test/changescene/SettingData.java
@@ -0,0 +1,139 @@
+package com.test.changescene;
+
+public class SettingData {
+    private String settingFileName;
+    private String userName;
+    private String releaseDate;
+    private String buildName;
+    private String serverName;
+    private String sdk;
+    private String slotName;
+    private String internalSlot;
+    private String url;
+    private String status;
+    private String environment;
+    private String issueType;
+    private String testUser;
+    private String language;
+    private String browser;
+
+    public String getSettingFileName() {
+        return settingFileName;
+    }
+
+    public void setSettingFileName(String settingFileName) {
+        this.settingFileName = settingFileName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getBuildName() {
+        return buildName;
+    }
+
+    public void setBuildName(String buildName) {
+        this.buildName = buildName;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getSdk() {
+        return sdk;
+    }
+
+    public void setSdk(String sdk) {
+        this.sdk = sdk;
+    }
+
+    public String getSlotName() {
+        return slotName;
+    }
+
+    public void setSlotName(String slotName) {
+        this.slotName = slotName;
+    }
+
+    public String getInternalSlot() {
+        return internalSlot;
+    }
+
+    public void setInternalSlot(String internalSlot) {
+        this.internalSlot = internalSlot;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public String getIssueType() {
+        return issueType;
+    }
+
+    public void setIssueType(String issueType) {
+        this.issueType = issueType;
+    }
+
+    public String getTestUser() {
+        return testUser;
+    }
+
+    public void setTestUser(String testUser) {
+        this.testUser = testUser;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
